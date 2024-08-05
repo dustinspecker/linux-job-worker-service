b192ebf227e9eeb38830bcf12978dacfab818873
Dustin Specker <DustinSpecker@DustinSpecker.com>
2024-08-05 18:21:39 -0500

    fix(internal/output_reader): return new bytes ASAP
    Before, Output's ReadAt would block until the buffer was full. This
    prevented sending bytes that were available until the buffer was full or
    the output was closed.
    
    Now, Output no longer implements io.ReaderAt, but has new methods,
    ReadPartial and Wait.
    
    OutputReader now attempts to use ReadAt. If any bytes are read then
    Read will return them. If zero bytes are read then OutputReader
    will wait for changes to Output before re-reading again.

--- internal/output_reader.go
+++ internal/output_reader.go
@@ -22,15 +22,30 @@ func NewOutputReader(output *Output) *OutputReader {
 }
 
 // Read reads from the Output and returns the number of bytes read and an error if any.
+// Read will wait for changes to the Output if no content is available to read.
 // Read returns EOF if the Output is closed and all the content has been read.
 func (outputReader *OutputReader) Read(buffer []byte) (int, error) {
 	if outputReader.output == nil {
 		return 0, ErrOutputMissing
 	}
 
-	bytesRead, err := outputReader.output.ReadAt(buffer, outputReader.readIndex)
+	if len(buffer) == 0 {
+		return 0, nil
+	}
+
+	bytesRead, err := outputReader.output.ReadPartial(buffer, outputReader.readIndex)
 	if err != nil {
 		return bytesRead, err
+	}
+
+	// If bytesRead is zero then wait for changes to the Output and read again.
+	if bytesRead == 0 {
+		outputReader.output.Wait(outputReader.readIndex)
+
+		bytesRead, err := outputReader.output.ReadPartial(buffer, outputReader.readIndex)
+		if err != nil {
+			return bytesRead, err
+		}
 	}
 
 	outputReader.readIndex += int64(bytesRead)
